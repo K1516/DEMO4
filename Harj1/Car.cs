3025c4cd0120e2ee2780131c4627bc30f973c132
K1516 <k1516@student.jamk.fi>
2016-01-20 15:18:38 +0200

    Upgrading my VS skills

---
+++ Harj1/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harj1
+{
+    class Car
+    {
+        //mas speed
+        private readonly int maxSpeed = 200;
+        //member field
+        private int speed;
+        // property
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                if (value <= maxSpeed) speed = value;
+                else
+                {
+                    Console.WriteLine("Too much speed - set to max!");
+                    speed = maxSpeed;
+                }
+            }
+        }
+
+    }
+}
---
+++ Harj1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Speed = 1232;
+            Console.WriteLine(" Car speed is " + car.Speed);
+            Console.ReadLine();
+        }
+    }
+}
