9e6eb33ab3922142fb871b34c7026e8d9d173251
Risto Laine <k1516@student.jamk.fi>
2016-01-25 20:32:43 +0200

    Adding homework, not ready yet

---
+++ Kirjahylly/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kirjahylly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Speaker speaker = new Speaker();
+            Console.WriteLine("Turn speakers on? 1 = YES, 0 = NO");
+            string rivi = Console.ReadLine();
+            int luku1 = int.Parse(rivi);
+            speaker.ONOFF = luku1;
+            if (luku1 == 1)
+            {
+                Console.WriteLine("Speakers turned on!");
+            }
+            else
+            {
+                Console.WriteLine("Speakers turned off! Enjoy your silence...");
+            }
+            Console.WriteLine("Set volume (0-10)");
+            string rivi2 = Console.ReadLine();
+            int luku2 = int.Parse(rivi2);
+            speaker.Volume = luku2;
+            Console.WriteLine("Volume is now on " + speaker.Volume + " , enjoy!");
+            Console.ReadLine();
+
+            Console.WriteLine("Choose u plug in: 1 = BLUETHOOT, 2 = AUX");
+            string rivi3 = Console.ReadLine();
+            int luku3 = int.Parse(rivi3);
+            
+
+        }
+    }
+}
---
+++ Kirjahylly/Speaker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kirjahylly
+{
+    class Speaker
+    {
+        private readonly int maxvolume = 10;
+        private readonly int minvolume = 0;
+        private int volume;
+        private readonly int onoff = 1;
+        private readonly int onff1 = 0;
+        private int onOFF;
+
+        public int ONOFF
+        {
+            get
+            {
+                return onOFF;
+            }
+            set
+            {
+                if (value >= 0 && value <= 1) onOFF = value;
+                else
+                {
+                    Console.WriteLine("Wrong value, speaker turned on! Volume set max, have fun!");
+                    onOFF = 1;
+                    volume = maxvolume;
+                }
+            }
+        }
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value <= 10 && value >= 0) volume = value;
+                else
+                {
+                    Console.WriteLine("Wrong volume, please try again! Volume set to 4.");
+                    volume = 4;
+                }
+            }
+        }
+
+
+    }
+}
